c0c0f415d9367942645d179056f71bb456fd7efd
farheen-Digitalks <farheendigitalks@gmail.com>
2026-01-24 17:38:50 +0530

    `Update axios post endpoint in loginUser function`

--- app/services/authService.ts
+++ app/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const loginUser = async( data: { email: string, password: string } ) => {
-    const res = await axios.post('/api/login', data);
+    const res = await axios.post('/login', data);
     return res
 }
